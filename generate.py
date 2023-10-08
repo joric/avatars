f6064c5386c915973e089e1018559bf5dc6193f1
joric <joric@users.noreply.github.com>
2023-10-08 09:58:26 +0500

    updated files

--- generate.py
+++ generate.py
@@ -10,14 +10,11 @@ h,l,s = m[25]<<8|m[26]<<4|m[27],m[30]<<4|m[31],m[28]<<4|m[29]
 rgb = colorsys.hls_to_rgb(h/16/256,(960-l)/5/256,(832-s)/5/256)
 fg = tuple(map(lambda x:round(x*255),rgb))
 bg = (240, 240, 240)
-
 img = Image.new('RGB', (size, size), bg)
 r = size // (5 + 1)
 box = lambda img,x,y,r,c: any(img.putpixel((i,j),c) for i in range(x,x+r) for j in range(y,y+r))
-
 for y in range(5):
     for x in range(5):
         if m[[2,1,0,1,2][x]*5+y]%2==0:
             box(img, x*r+r//2, y*r+r//2, r, fg)
-
 img.save(f'{id}.png')
